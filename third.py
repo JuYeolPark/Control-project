fd75a7bb2ca9b950bfdedcd8ad3dba832286f665
chun2435 <65524778+WJbayway@users.noreply.github.com>
2020-05-22 12:25:56 +0900

    현대제어 박주열

---
+++ third.py
@@ -0,0 +1,17 @@
+import first
+from second import add
+
+print(__name__)
+
+
+result = add(5,6)
+print(result)
+
+hyori = first.Woman(45,165,120)
+
+hyori.show()
+hyori.eat()
+hyori.sleep()
+hyori.study()
+
+hyori.show()
